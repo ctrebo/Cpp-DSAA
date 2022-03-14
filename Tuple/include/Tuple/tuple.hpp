3299abc0b53961e4ea8d86e829ddfdd4a906ebb1
Christoffer Trebo <christoffer.trebo@gmail.com>
2022-03-14 15:44:51 +0100

    implement 'operator++' function

--- Tuple/include/Tuple/tuple.hpp
+++ Tuple/include/Tuple/tuple.hpp
@@ -49,6 +49,12 @@ namespace ds {
         Tuple(CArgs&&... args): TupleRecurBase<0, L, types...>{std::forward<CArgs>(args)...}
         {
         }
+
+        template<class... Args>
+        friend bool operator==(Tuple<Args...>& t1, Tuple<Args...>& t2) {
+            return compare_tuple<sizeof...(Args) - 1>(t1, t2);
+        }
+
     };
 
     // Template deduction guide for Tuple class
@@ -72,6 +78,16 @@ namespace ds {
     template<std::size_t index, class... Args>
     auto& get(Tuple<Args...>& t) {
         return (static_cast<TupleImpl<index, ExtractTypeAt_t<index, Args...>>&>(t)).get();
+    }
+
+    template<std::size_t index, class... Args>
+    bool compare_tuple(Tuple<Args...>& t1, Tuple<Args...>& t2) {
+        if constexpr (index == 0) {
+            return (get<0>(t1) == get<0>(t2));
+        } else {
+            return (get<index>(t1) == get<index>(t2)) && compare_tuple<index -1>(t1, t2);
+        }
+
     }
 
 }
