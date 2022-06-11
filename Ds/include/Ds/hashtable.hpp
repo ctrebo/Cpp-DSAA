4c0570da7c4d3faba0e06cb14cf27e015e11e35a
Christoffer Trebo <christoffer.trebo@gmail.com>
2022-06-11 10:08:42 +0200

    add new constructors, functions and tests to hashtable

--- Ds/include/Ds/hashtable.hpp
+++ Ds/include/Ds/hashtable.hpp
@@ -2,6 +2,7 @@
 #define HASH_TABLE_HPP
 
 #include "list.hpp"
+#include "concepts.hpp"
 
 #include <utility>
 #include <cstddef>
@@ -13,8 +14,18 @@
 #include <memory>
 #include <cassert>
 #include <exception>
+#include <optional>
+#include <type_traits>
+#include <iterator>
+
 
 namespace ds {
+
+template<class Key, class T>
+class HashTable;
+
+template<class KeyF, class TF>
+void swap(HashTable<KeyF, TF>& first, HashTable<KeyF, TF>& second);
 
 template<class Key, class T>
 class HashTable {
@@ -30,12 +41,17 @@ public:
     HashTable();
     HashTable(size_type size);
     HashTable(std::initializer_list<value_type> iList);
+    template<class InputIt>
+    requires is_it<InputIt>
+    HashTable(InputIt first, InputIt last);
+    HashTable(const HashTable& ht);
+    HashTable(HashTable&& other) noexcept;
 
     // Size functions
     size_type size() const;
     size_type capacity() const;
     bool empty() const;
-    bool exists(const key_type& key);
+    bool exists(const key_type& key) const;
 
 
     // Modifiers
@@ -43,16 +59,21 @@ public:
     bool insert(const key_type& key, const mapped_type&& value);
     T& operator[](const key_type& key);
     const T& operator[](const key_type& key) const;
-    
+    void erase(const key_type& key);
 
+    HashTable<Key, T>& operator=(HashTable<Key, T> other) noexcept;
+
+    template<class KeyF, class TF>
+    friend void swap(HashTable<KeyF, TF>& first, HashTable<KeyF, TF>& second);
+    
 private:
     size_type capacity_;
     size_type size_;
-    std::vector<value_type> arr_;
+    std::vector<std::optional<value_type>> arr_;
 
 private:
     static size_type addUpNums(size_type num);
-    size_type hashFunction(const Key& key);
+    size_type hashFunction(const Key& key) const;
     size_type moduloIndex(size_type index) const;
     template<class Type>
     bool insertPriv(const Key& key, Type&& value);
@@ -60,7 +81,7 @@ private:
 };
 
 template<class Key, class T>
-HashTable<Key, T>::HashTable(): capacity_ {10000}, arr_(capacity_), size_(0) {}
+HashTable<Key, T>::HashTable(): capacity_ {0}, arr_(capacity_), size_(0) {}
 
 template<class Key, class T>
 HashTable<Key, T>::HashTable(size_type size): capacity_ {size}, arr_(capacity_), size_ {0} {}
@@ -70,6 +91,36 @@ HashTable<Key, T>::HashTable(std::initializer_list<value_type> iList): capacity_
     for(const auto& elem: iList) {
         insertPriv(elem.first, elem.second);
     }
+}
+
+template<class Key, class T>
+template<class InputIt>
+requires is_it<InputIt>
+HashTable<Key, T>::HashTable(InputIt first, InputIt last): capacity_ {std::distance(first, last) > 10000 ? static_cast<size_type>(std::distance(first, last) * 2) : 10000}, size_ {0}, arr_(capacity_) {
+    if constexpr(std::is_same_v<std::remove_reference_t<decltype(*first)>, value_type>) {
+        for(auto it = first; it != last; ++it) {
+           const auto pair = *it;
+
+            insertPriv(pair.first, pair.second);
+        }
+    } else {
+        throw std::invalid_argument("Container has to be of type std::pair!");
+    }
+
+}
+
+template<class Key, class T>
+HashTable<Key, T>::HashTable(const HashTable& ht): 
+    capacity_ {ht.capacity_},
+    size_ {ht.size_},
+    arr_ {ht.arr_}
+{
+}
+
+
+template<class Key, class T>
+HashTable<Key, T>::HashTable(HashTable&& other) noexcept: HashTable() {
+    swap(*this, other); 
 }
 
 template<class Key, class T>
@@ -91,12 +142,14 @@ bool HashTable<Key, T>::empty() const {
 }
 
 template<class Key, class T>
-bool HashTable<Key, T>::exists(const key_type& key) {
-    size_type index {HashTable<Key, T>::hashFunction(key)};
+bool HashTable<Key, T>::exists(const key_type& key) const {
+    size_type index {hashFunction(key)};
     size_type old_index {index};
     while(true) {
-        if(key == arr_[moduloIndex(index)].first) {
-            return true;
+        if(arr_[moduloIndex(index)]) {
+            if(key == (*arr_[moduloIndex(index)]).first) {
+                return true;
+            }
         }
         if (index == old_index) {
             break;
@@ -108,27 +161,28 @@ bool HashTable<Key, T>::exists(const key_type& key) {
 
 template<class Key, class T>
 T& HashTable<Key, T>::operator[](const key_type& key) {
-    std::cout << "in here";
-    size_type index = HashTable<Key, T>::hashFunction(key);
+    size_type index = hashFunction(key);
     assert(size_ <= capacity_ && size_ >= 0 && "Something went wrong!");
     if (exists(key)) {
-        if(key == arr_[index].first) {
-            return arr_[index].second;
+        if(key == (*arr_[index]).first) {
+            return (*arr_[index]).second;
         } else {
             ++index;
             while(true) {
-                if(key == arr_[moduloIndex(index)].first) {
-                    return arr_[moduloIndex(index)].second;
+                if (arr_[moduloIndex(index)]) {
+                    if(key == (*arr_[moduloIndex(index)]).first) {
+                        return (*arr_[moduloIndex(index)]).second;
+                    }
                 }
                 ++index;
             }
         }
     } else {
         while(true) {
-            if(!(arr_[moduloIndex(index)].first)) {
-                arr_[moduloIndex(index)].first = key;
+            if(!(arr_[moduloIndex(index)])) {
+                arr_[moduloIndex(index)] = std::make_pair(key, mapped_type());
                 ++size_;
-                return (arr_[moduloIndex(index)].second);
+                return ((*arr_[moduloIndex(index)]).second);
             }
             ++index;
         }
@@ -138,22 +192,37 @@ T& HashTable<Key, T>::operator[](const key_type& key) {
 template<class Key, class T>
 const T& HashTable<Key, T>::operator[](const key_type& key) const {
     assert(size_ <= capacity_ && size_ >= 0 && "Something went wrong!");
-    size_type index = HashTable<Key, T>::hashFunction(key);
+    size_type index = hashFunction(key);
     if (!exists(key)) {
         throw std::invalid_argument("Key is not present in the Hashtable");
     }
 
-    if(key == arr_[index].first) {
-        return arr_[index].second;
-    } else {
-        ++index;
-        while(true) {
-            if(key == arr_[moduloIndex(index)].first) {
-                return arr_[moduloIndex(index)].second;
+    while(true) {
+        if(arr_[moduloIndex(index)]) {
+            if(key == (*arr_[moduloIndex(index)]).first) {
+                return (*arr_[moduloIndex(index)]).second;
             }
         }
+        ++index;
     }
-    
+}
+template<class Key, class T>
+void HashTable<Key, T>::erase(const key_type& key) {
+    if(!exists(key)) {
+        return;
+    }
+    size_type index {hashFunction(key)};
+    while(true) {
+        if(arr_[moduloIndex(index)]) {
+            if(key == (*arr_[moduloIndex(index)]).first) {
+                arr_[moduloIndex(index)].reset();
+                --size_;
+                return;
+            }
+        }
+        ++index;
+    }
+
 }
 
 template<class Key, class T>
@@ -179,7 +248,7 @@ HashTable<Key, T>::size_type HashTable<Key, T>::addUpNums(size_type num) {
 }
 
 template<class Key, class T>
-HashTable<Key, T>::size_type HashTable<Key, T>::hashFunction(const Key& key) {
+HashTable<Key, T>::size_type HashTable<Key, T>::hashFunction(const Key& key) const {
     return (HashTable<Key, T>::addUpNums(std::hash<T>{}(key)) % capacity_);
 }
 template<class Key, class T>
@@ -194,16 +263,31 @@ bool HashTable<Key, T>::insertPriv(const Key& key, Type&& value) {
         return false;
     }
     assert(size_ < capacity_ && "Array got no more space");
-    size_type index {HashTable<Key, T>::hashFunction(key)};
+    size_type index {hashFunction(key)};
     while(true) {
-        if (!(arr_[moduloIndex(index)].first)) {
-            arr_[moduloIndex(index)].first = key;  
-            arr_[moduloIndex(index)].second = std::forward<Type>(value);  
+        if (!(arr_[moduloIndex(index)])) {
+            arr_[moduloIndex(index)] = std::make_pair(key, std::forward<Type>(value));  
             ++size_;
             return true;
         }
         ++index;
     }
+}
+
+template<class Key, class T>
+HashTable<Key, T>& HashTable<Key, T>::operator=(HashTable<Key, T> other) noexcept {
+    swap(*this, other);
+
+    return *this;
+}
+
+template<class KeyF, class TF>
+void swap(HashTable<KeyF, TF>& first, HashTable<KeyF, TF>& second) {
+    using std::swap;
+
+    swap(first.size_, second.size_);
+    swap(first.capacity_, second.capacity_);
+    swap(first.arr_, second.arr_);
 }
 
 }
--- Ds/tests/hashtable_test.cpp
+++ Ds/tests/hashtable_test.cpp
@@ -8,10 +8,11 @@
 #include <functional>
 #include <initializer_list>
 #include <utility>
+#include <exception>
 
 
 TEST_CASE("Test own implemented Hashtable constructors") {
-    //HashTable(size_type size=10000);
+    //HashTable(size_type size);
     SECTION("Construct HashTable with size passed to constructor") {
         ds::HashTable<int, int> ht(20000);
         REQUIRE(ht.size()==0);
@@ -27,6 +28,39 @@ TEST_CASE("Test own implemented Hashtable constructors") {
         std::initializer_list<std::pair<int, int>> iList {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
         ds::HashTable<int, int> hs {iList};
         REQUIRE(hs.size() == iList.size());
+    }
+
+    //HashTable(const HashTable& ht);
+    SECTION("Initalize HashTable with copy of 'ht'") {
+        ds::HashTable<int, int> hs {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
+
+        ds::HashTable<int, int> ht {hs};
+        REQUIRE(ht.size() == hs.size());
+        REQUIRE(ht.capacity() == hs.capacity());
+    }
+
+    //template<class InputIt>
+    //HashTable(InputIt first, InputIt last);
+    SECTION("Input values between first and last") {
+        using hashtable = ds::HashTable<int, int>;
+        std::array<std::pair<int, int>, 6> arr {{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}};
+        hashtable ht(arr.begin(), arr.end());
+        REQUIRE(arr.size() == ht.size());
+        REQUIRE(ht[1] == 2);
+
+        std::array arr1 {1, 2, 3, 4, 5, 6, 7};
+        CHECK_THROWS_AS(hashtable(arr1.begin(), arr1.end()),  std::invalid_argument);
+
+    }
+
+    //HashTable(const HashTable&& other) noexcept;
+    SECTION("Move construct HashTable from 'other'") {
+        ds::HashTable<int, int> hs {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
+
+        ds::HashTable<int, int> ht {std::move(hs)};
+        REQUIRE(ht.size() == 6);
+        REQUIRE(hs.size() == 0);
+
     }
 }
 
@@ -67,7 +101,36 @@ TEST_CASE("Test own implemented HashTables modifier functions") {
     //T& operator[](const key_type& key);
     SECTION("Return reference to element") {
         ht[7] = 8;
-        REQUIRE(ht.size() == 7);
-        REQUIRE(ht[7] == 8);
+        CHECK(ht.size() == 7);
+        CHECK(ht[7] == 8);
+
+
     }
+
+    //const T& operator[](const key_type& key) const;
+    SECTION("Return const reference to element") {
+        const ds::HashTable<int, int> const_ht {{0, 1}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
+        
+        CHECK(const_ht[0] == 1);
+        CHECK_THROWS_AS(const_ht[100],  std::invalid_argument);
+    }
+
+    //void erase(const key_type& key);
+    SECTION("Erase element from HashTable") {
+        ht.erase(0);
+        CHECK(ht.size() == 5);
+        CHECK(!ht.exists(0));
+    }
+
+    //HashTable<Key, T>& operator=(HashTable<Key, T> other) noexcept;
+    SECTION("Assing value of 'other' to '*this'") {
+        ds::HashTable<int, int> ht1;
+        ht1 = ht;
+        CHECK(ht.size() == ht1.size());
+        CHECK(ht[3] == ht1[3]);
+
+        ht1 = std::move(ht);
+        CHECK(ht.size() != ht1.size());
+    }
+
 }
